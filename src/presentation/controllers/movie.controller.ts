cc95375e82361591b1879ffb30edb697d3a56ddd
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2025-02-11 22:01:27 +0700

    finish upload route

--- src/presentation/controllers/movie.controller.ts
+++ src/presentation/controllers/movie.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, UseInterceptors, Post, Body, Req, UseGuards, UploadedFile } from '@nestjs/common';
+import { Controller, UseInterceptors, Post, Body, Req, UseGuards, UploadedFile, Response } from '@nestjs/common';
 import { ResponseType } from '../../interface/response.interface';
 import { MovieServices } from '../../core/movie/services/movie.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MovieGuard } from 'src/core/movie/movie.guard';
 import { S3Service } from 'src/Infrastructure/s3/s3.service';
 import { RAW_MOVIE_BUCKET } from 'src/core/movie/movie.config';
+import { Response as ExpressResponse } from 'express';
 import path from 'path';
 
 
@@ -23,14 +24,18 @@ export class MovieController {
   async uploadMovie(@Body() body: {
     name: string;
     description: string
-  }, @Req() req: Request, @UploadedFile() file: Express.Multer.File): Promise<string> {
+  }, @Req() req: Request, @UploadedFile() file: Express.Multer.File, @Response() res: ExpressResponse ) { 
     const uploadTicket = req.headers['x-upload-ticket']
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     const extName = path.extname(file.originalname)
     const movileName = uniqueSuffix.trimEnd() + extName 
     await this.S3services.upLoadToS3(RAW_MOVIE_BUCKET, movileName, file.buffer);
-    await this.movieServices.uploadMovie(`${process.env.S3_SERVICE_ENDPOINT}/${RAW_MOVIE_BUCKET}/${movileName}`, 'video', uploadTicket)
-    return 'l'
+     const uploadResult = await this.movieServices.uploadMovie(`${process.env.S3_SERVICE_ENDPOINT}/${RAW_MOVIE_BUCKET}/${movileName}`, 'video', uploadTicket)
+     const response : ResponseType = {
+      message: uploadResult.message,
+      data: null,
+     }
+    return  res.status(uploadResult.status).send(response)
   }
 
   @Post('upload/registerUploadTicket')
