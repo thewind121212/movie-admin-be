ec1b1c7625501ba4e97393cfbc37699ce278f16c
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2025-02-19 21:33:23 +0700

    add check exist user in user db

--- src/core/user/guards/validateJWTRegisterRequest.guard.ts
+++ src/core/user/guards/validateJWTRegisterRequest.guard.ts
@@ -3,13 +3,15 @@
 import { Injectable, CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
 import { UserSecurity } from '../security/user.security';
 import { HttpStatusCode } from 'axios';
+import { UserRepositories } from '../repositories/user.repositories';
 
 
 @Injectable()
 export class ValidateTokenRegisterRequestGuard implements CanActivate {
 
     constructor(
-        private readonly userSecurity: UserSecurity
+        private readonly userSecurity: UserSecurity,
+        private readonly userRepositories: UserRepositories
     ) {
 
     }
@@ -17,6 +19,11 @@ export class ValidateTokenRegisterRequestGuard implements CanActivate {
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
+
+        // check is user already exist
+
+
+
         const { token } = request.body
         if (!token) {
             throw new HttpException(
@@ -29,6 +36,8 @@ export class ValidateTokenRegisterRequestGuard implements CanActivate {
             )
         }
 
+
+
         const { isValid, email } = this.userSecurity.verifyJWT(token)
         if (!isValid) {
             throw new HttpException(
@@ -36,6 +45,29 @@ export class ValidateTokenRegisterRequestGuard implements CanActivate {
                     status: 'Forbidden',
                     data: null,
                     message: 'Invalid token please try again'
+                },
+                HttpStatusCode.Forbidden
+            )
+        }
+
+        if (!email) {
+            throw new HttpException(
+                {
+                    status: 'Forbidden',
+                    data: null,
+                    message: 'Invalid token please try again'
+                },
+                HttpStatusCode.Forbidden
+        )}
+    
+        const isUserExist = await this.userRepositories.findUser(email)
+
+        if (isUserExist) {
+            throw new HttpException(
+                {
+                    status: 'Forbidden',
+                    data: null,
+                    message: 'User already exist'
                 },
                 HttpStatusCode.Forbidden
             )
